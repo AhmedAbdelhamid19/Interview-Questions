4f4740b3072a379cf4dca0f9d992e2aaa58604ac
Ahmed Abdelhamid <94197524+AhmedAbdelhamid19@users.noreply.github.com>
2025-10-04 20:24:42 +0300

    Create 198. House Robber.cpp

---
+++ NeetCode 150/198. House Robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
+        for(int i=0; i<n; i++) {
+            dp[i] = nums[i] + (i - 2 >= 0 ? dp[i - 2] : 0);
+            if(i - 1 >= 0) dp[i] = max(dp[i], dp[i - 1]);
+        }
+        return dp[n - 1];
+    }
+};
