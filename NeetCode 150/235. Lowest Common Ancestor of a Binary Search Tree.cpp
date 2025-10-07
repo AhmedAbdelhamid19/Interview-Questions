73358bd1fa4203d7d920d56c5a040c01da5ae8ff
Ahmed Abdelhamid <94197524+AhmedAbdelhamid19@users.noreply.github.com>
2025-10-07 19:25:02 +0300

    Create 235. Lowest Common Ancestor of a Binary Search Tree.cpp

---
+++ NeetCode 150/235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -0,0 +1,73 @@
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        ListNode *pPtr = new ListNode(root->val);
+        ListNode *qPtr = new ListNode(root->val);
+        pPtr->treeNode = qPtr->treeNode = root;
+        dfs(root, p->val, q->val, 0, pPtr, qPtr);
+
+        while(pPtr->dep > qPtr->dep) {
+            pPtr = pPtr->pre;
+        }
+        while(qPtr->dep > pPtr->dep) {
+            qPtr = qPtr->pre;
+        }
+
+        while(qPtr->val != pPtr->val) {
+            qPtr = qPtr->pre;
+            pPtr = pPtr->pre;
+        }
+
+        return pPtr->treeNode;
+    }
+private:
+    struct ListNode {
+        int val, dep;
+        ListNode *pre, *left, *right;
+        TreeNode* treeNode;
+        ListNode(int x): val(x), dep(0), pre(nullptr), left(nullptr), right(nullptr) {};
+    };
+    void dfs(TreeNode *&cur, int p, int q, int dep, ListNode *&pPtr, ListNode *&qPtr) {
+        if(cur == nullptr) return;
+
+        if(cur->left != nullptr) {
+            if(pPtr->val != p) {
+                pPtr->left = new ListNode(cur->left->val);
+                pPtr->left->pre = pPtr;
+                pPtr->left->dep = dep + 1;
+                pPtr->left->treeNode = cur->left;
+                pPtr = pPtr->left;
+            }
+            if(qPtr->val != q) {
+                qPtr->left = new ListNode(cur->left->val);
+                qPtr->left->pre = qPtr;
+                qPtr->left->dep = dep + 1;
+                qPtr->left->treeNode = cur->left;
+                qPtr = qPtr->left;
+            }
+            dfs(cur->left, p, q, dep + 1, pPtr, qPtr);
+            if(pPtr->val != p) pPtr = pPtr->pre;
+            if(qPtr->val != q) qPtr = qPtr->pre;
+        }
+        if(cur->right != nullptr) {
+            if(pPtr->val != p) {
+                pPtr->right = new ListNode(cur->right->val);
+                pPtr->right->pre = pPtr;
+                pPtr->right->dep = dep + 1;
+                pPtr->right->treeNode = cur->right;
+                pPtr = pPtr->right;
+            }
+            if(qPtr->val != q) {
+                qPtr->right = new ListNode(cur->right->val);
+                qPtr->right->pre = qPtr;
+                qPtr->right->dep = dep + 1;
+                qPtr->right->treeNode = cur->right;
+                qPtr = qPtr->right;
+            }
+            dfs(cur->right, p, q, dep + 1, pPtr, qPtr);
+            if(pPtr->val != p) pPtr = pPtr->pre;
+            if(qPtr->val != q) qPtr = qPtr->pre;
+        }
+    }
+};
