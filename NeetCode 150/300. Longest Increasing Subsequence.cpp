033773198b812766fba67e7dbcce964b81cb6aaf
Ahmed Abdelhamid <94197524+AhmedAbdelhamid19@users.noreply.github.com>
2025-10-23 01:20:22 +0300

    Create 300. Longest Increasing Subsequence.cpp

---
+++ NeetCode 150/300. Longest Increasing Subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int> nums) {
+        int n = nums.size();
+        vector<int>lis;
+        for(int i=0; i<n; i++) {
+            int idx = std::lower_bound(lis.begin(), lis.end(), nums[i]) - lis.begin();
+            if(idx == lis.size()) {
+                lis.push_back(nums[i]);
+            } else {
+                lis[idx] = nums[i];
+            }
+        }
+        return lis.size();
+    }
+};
